704940ca47ce2a5fc52a11ef5f95cac2a598f0c8
ddurmala <169076397+ddurmala@users.noreply.github.com>
2024-08-30 11:38:58 -0400

    about to add cloudinary -package already installed

---
+++ client/src/pages/UserDashboard.jsx
@@ -0,0 +1,67 @@
+import ArtItem from "../components/ArtItem";
+
+import { useState } from "react"
+import { useMutation, useQuery } from "@apollo/client"
+
+import { ADD_ARTWORK, DELETE_ARTWORK } from "../graphql/mutations"
+import { GET_USER_ARTWORK, GET_ALL_ARTWORK } from "../graphql/queries"
+
+
+const initialFormData = {
+    name: '',
+    weapon: '',
+    headbandColor: ''
+}
+
+function Dashboard() {
+    const [formData, setFormData] = useState(initialFormData)
+    const [addArtwork] = useMutation(ADD_ARTWORK, {
+        variables: formData,
+        refetchQueries: [GET_USER_ARTWORK, GET_ALL_ARTWORK]
+    })
+    const [deleteArtwork] = useMutation(DELETE_ARTWORK, {
+        refetchQueries: [GET_USER_ARTWORK, GET_ALL_ARTWORK]
+    })
+    const { data: artworkData } = useQuery(GET_USER_ARTWORK)
+
+    if (artworkDataData) {
+        console.log(addArtworkData)
+    }
+
+    const handleInputChange = event => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleSubmit = async event => {
+        event.preventDefault()
+
+        const res = await addArtwork()
+
+        console.log(res)
+
+        setFormData({
+            ...initialFormData
+        })
+    }
+
+    const handleArtwork = async (id) => {
+        await deleteArtwork({
+            variables: {
+                artwork_id: id
+            }
+        })
+    }
+
+    return (
+
+
+
+
+        
+
+    )
+
+}
